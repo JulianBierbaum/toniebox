c7ced0408d2f52529f081352c59b66cc9afa22c0
Julian Bierbaum <bierbaumjulian@gmail.com>
2024-12-29 21:05:15 +0100

    changed buttons

--- buttons.py
+++ buttons.py
@@ -1,10 +1,12 @@
 from gpiozero import Button
+from signal import pause
 
 up = Button(24)
 down = Button(23)
 confirm = Button(22)
 
-while 1:
-    up.when_pressed = print("up pressed")
-    down.when_pressed = print("down pressed")
-    confirm.when_pressed = print("confirm pressed")
+up.when_pressed = print("up pressed")
+down.when_pressed = print("down pressed")
+confirm.when_pressed = print("confirm pressed")
+
+pause()
